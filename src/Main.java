029eeba6294587876848f2fa8f964a13070fc5ff
Taha Sari <saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.com>
2022-10-30 18:07:55 +0300

    commit2 bugs fixed

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,34 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        int mesafe,tercih,yas;
-        double ucret=0.0;
-        System.out.println("Lütfen gideceğiniz mesafeyi kilometre cinsinden giriniz:");
-        mesafe= scanner.nextInt();
-        System.out.println("Lütfen yolculuk tipinizi seçiniz:\n1.Gidiş-Dönüş\n2.Tek Yön");
-        tercih= scanner.nextInt();
-        System.out.println("Lütfen yaşınızı giriniz:");
-        yas= scanner.nextInt();
-        if(tercih==1){
-          ucret=mesafe*0.10;
-          ucret=ucret-(ucret*0.2);
-          if(yas<12){ucret=ucret-(ucret/2);
-          } else if (12<=yas && yas<24) {
-              ucret=ucret-(ucret*0.1);
-          } else if (yas>65) {
-              ucret=ucret-(ucret*0.3);
+        int distance,choice,age;
+        double fee=0.0;
+        System.out.println("Please enter your distance in kilometers:");
+        distance= scanner.nextInt();
+        System.out.println("Please select your trip type:\n1. Round trip\n2.One Way");
+        choice= scanner.nextInt();
+        System.out.println("Please enter your age:");
+        age= scanner.nextInt();
+        if(choice==1){
+          fee=distance*0.10;
+          fee=fee-(fee*0.2);
+          if(age<12){fee=fee-(fee/2);
+          } else if (12<=age && age<24) {
+              fee=fee-(fee*0.1);
+          } else if (age>65) {
+              fee=fee-(fee*0.3);
           }
         }
-        else if(tercih==2){
-            ucret=mesafe*0.10;
-            if(yas<12){ucret=ucret-(ucret/2);
-            } else if (12<=yas && yas<24) {
-                ucret=ucret-(ucret*0.1);
-            } else if (yas>65) {
-                ucret=ucret-(ucret*0.3);
+        else if(choice==2){
+            fee=distance*0.10;
+            if(age<12){fee=fee-(fee/2);
+            } else if (12<=age && age<24) {
+                fee=fee-(fee*0.1);
+            } else if (age>65) {
+                fee=fee-(fee*0.3);
             }
-   } else if (yas<0 || mesafe<0 || tercih>2) {System.out.println("Hatalı değer girildi.");
+   } else if (age<0 || distance<0 || choice>2) {System.out.println("Wrong value entered.");
         }
-System.out.println("Ücretiniz:"+ucret+" "+"TL");
+System.out.println("Your fee:"+fee+" "+"TL");
     }
 }
